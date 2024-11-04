ebaeb887ed6c861e0e5c6ed266af90d25b68fa32
stl-alextaciuk <alex.taciuk@streetlightdata.com>
2024-11-04 11:15:48 -0800

    further modifications to the axes

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name='spc-plotly',
+    version='0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+)
